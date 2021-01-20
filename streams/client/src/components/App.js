94549fcc56a72e06d58099efdfe9b2f6fa85b824
Saran <romansaran446@gmail.com>
2021-01-20 15:17:15 +0530

    browser router -hash router - memory router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,31 @@
 import React from  'react';
+import {BrowserRouter,Link,Route} from 'react-router-dom';
+
+
+const PageOne = () => {
+    return  ( 
+    <div>Page
+    <Link to="/pagetwo">pagetwo</Link>
+    </div>
+    
+    );
+}
+
+const PageTwo  =() => {
+    return <div>Page Two</div>
+}
 
 const App = () => {
-    return <div> App</div>
+    return  (
+    
+        <BrowserRouter>
+        <div>
+            <Route path="/" exact component={PageOne}/>        
+            <Route path="/pagetwo" component={PageTwo}/>
+            </div>
+        </BrowserRouter>
+    
+    );
 };
 
 export default App;
