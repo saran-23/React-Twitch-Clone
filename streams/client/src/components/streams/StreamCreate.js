1816229eb8365039c4bcf216944d1b7c8e72f1e7
Saran <romansaran446@gmail.com>
2021-01-21 18:48:11 +0530

    form validation

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,16 +2,51 @@ import React from 'react';
 import {Field,reduxForm} from 'redux-form';    //i using rf 
  
 class SteamCreate extends React.Component {
+renderInput({input,label,meta}) {
+    
+    return (
+    <div className="field">
+    <label>{label}</label>
+  < input {...input} />
+        <div>{meta.error}</div>
+  </div>
+);
+}
+    
+    onSubmit(formValues)  {
+    console.log(formValues);
+}
+
     render() {
         return (
-            <form>
-                <Field  name="Title"/>
-                <Field name ="description" />
+            <form 
+            onSubmit={this.props.handleSubmit(this.onSubmit)}
+            className="ui form"
+            >
+                <Field  name="Title" component={this.renderInput} label="Enter Title"/>
+                <Field name ="description" component={this.renderInput}  label="Enter Description"/>
+                <button className="ui button primary">
+                    Submit
+                </button>
             </form>
         );
     }
 }
 
+const validate =  formValues => {
+    const errors = {};
+
+    if(!formValues.Title) {
+        errors.Title="Please Enter the Title"
+    }
+    if(!formValues.description) {
+        errors.description ="Please Enter a Description";
+    }
+    return errors;
+}
+
+
 export default reduxForm({
-    form: 'steamCreate'
+    form: 'steamCreate',
+    validate,
 })(SteamCreate);
