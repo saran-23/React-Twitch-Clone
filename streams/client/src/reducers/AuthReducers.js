1a6aa5958b1ca02d592fa8f1d0a981082a13cf7a
Saran <romansaran446@gmail.com>
2021-01-21 14:43:45 +0530

    refactoring SIGNIN-OUT

--- streams/client/src/reducers/AuthReducers.js
+++ streams/client/src/reducers/AuthReducers.js
@@ -1,3 +1,4 @@
+import {SIGN_IN , SIGN_OUT } from '../actions/types';
 const INITIAL_STATE = {
     isSignedIn:null
 };
@@ -5,10 +6,10 @@ const INITIAL_STATE = {
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = INITIAL_STATE,action) => {
     switch (action.type) {
-        case 'SIGN_IN':
+        case SIGN_IN:    //variable
                 return{...state,isSignedIn:true,};
 
-        case  'SIGN_OUT' :
+        case  SIGN_OUT:     // variable 
                 return {...state,isSignedIn:false};
         default: 
         return state;
