96bd95775fde8beaa8e1676eaaa33e717d2c4caa
Saran <romansaran446@gmail.com>
2021-01-21 15:15:23 +0530

    i redux dev tools

--- streams/client/src/reducers/AuthReducers.js
+++ streams/client/src/reducers/AuthReducers.js
@@ -1,16 +1,17 @@
 import {SIGN_IN , SIGN_OUT } from '../actions/types';
 const INITIAL_STATE = {
-    isSignedIn:null
+    isSignedIn:null,
+    userId:null,
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = INITIAL_STATE,action) => {
     switch (action.type) {
         case SIGN_IN:    //variable
-                return{...state,isSignedIn:true,};
+                return{...state,isSignedIn:true, userId: action.payload};
 
         case  SIGN_OUT:     // variable 
-                return {...state,isSignedIn:false};
+                return {...state,isSignedIn:false,userId:null};
         default: 
         return state;
     }
