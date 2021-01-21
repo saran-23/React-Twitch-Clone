dcc7b05bffa230cc8cc4ca8f0335c166e7b45037
Saran <romansaran446@gmail.com>
2021-01-21 11:07:58 +0530

    redux-setup

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -20,6 +20,12 @@ state = {isSignedIn:null}; //dont know user signed in ror not
         onAuthChange = ()  => {
             this.setState({isSignedIn: this.auth.isSignedIn.get()});
         }
+        onSignInClick = () => {
+            this.auth.signIn();
+        };
+        onSignOutClick = ()  => {
+            this.auth.signOut();
+        };
 
 
 
@@ -28,14 +34,14 @@ state = {isSignedIn:null}; //dont know user signed in ror not
             return  null;
         } else if (this.state.isSignedIn) {
             return (
-                <button className="ui red google button">
+                <button onClick={this.onSignOutClick} className="ui red google button">
                     <i className="google icon"/>
                         Sign Out
                 </button>
             )
         } else {
             return (
-            <button className="ui red google button">
+            <button onClick={this.onSignInClick} className="ui red google button">
                 <i className="google icon"/>
                 Sign In with Google
             </button>
