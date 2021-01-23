7ede9a38627a7cdf79f91b8f7b6d5c17426f3a26
Saran <romansaran446@gmail.com>
2021-01-23 14:30:19 +0530

    rendered stream content by id

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const StreamEdit= (props) => {
-    console.log(props);
+import {fetchStream} from '../../actions';
+
+class  StreamEdit extends React.Component {
+componentDidMount () {
+    this.props.fetchStream(this.props.match.params.id);
+}
+
+    render() {
+        if(!this.props.stream){
+            return <div>loading...</div> 
+        }
     return  (
-        <div>StreamEdit</div>
+        <div>{this.props.stream.title}</div>
     );
-};
+    }
+}
 
 const mapStateToProps = (state,ownProps) => {
   
     return {stream:state.stream[ownProps.match.params.id]};
 }
 
-export default  connect (mapStateToProps)(StreamEdit);
+export default  connect (mapStateToProps,{fetchStream})(StreamEdit);
