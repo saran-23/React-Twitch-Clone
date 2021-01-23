920508ace1286360f346131d10f07684ae339b16
Saran <romansaran446@gmail.com>
2021-01-23 17:12:36 +0530

    fetching streamdelete even reload

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -14,9 +14,9 @@ import {fetchStreams} from '../../actions';
                     <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
                         Edit
                     </Link>
-                    <button className="ui button negative">
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
                         Delete
-                    </button>
+                    </Link>
                 </div>
                 );
             }   
