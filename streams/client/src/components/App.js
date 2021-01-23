c0a1cf597981478c15a079f16747c79b7324c8e6
Saran <romansaran446@gmail.com>
2021-01-23 11:59:50 +0530

    using programmatic navigation

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,6 +1,6 @@
-/* eslint-disable no-unused-vars */
+
 import React from  'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {Router,Route} from 'react-router-dom';
 
 
 import StreamCreate from './streams/StreamCreate';
@@ -9,13 +9,14 @@ import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 const App = () => {
     return  (
     
             <div className ="ui container">
-
-        <BrowserRouter>
+                    
+        <Router history={history}>                                                         
         <div>
         <Header />
             <Route path ="/" exact component = {StreamList}/>
@@ -24,7 +25,7 @@ const App = () => {
             <Route path ="/streams/delete" exact component = {StreamDelete}/>
             <Route path ="/streams/show" exact component = {StreamShow}/>
             </div>
-        </BrowserRouter>
+        </Router>
     
         </div>
     );
