fecbaa124e0626344b06e0240a2652111f300ef4
Saran <romansaran446@gmail.com>
2021-01-23 18:42:42 +0530

    react-router: <Swich> it display's only one route link

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,6 +1,6 @@
 
 import React from  'react';
-import {Router,Route} from 'react-router-dom';
+import {Router,Route,Switch} from 'react-router-dom';
 
 
 import StreamCreate from './streams/StreamCreate';
@@ -19,12 +19,15 @@ const App = () => {
         <Router history={history}>                                                         
         <div>
         <Header />
+        <Switch>
             <Route path ="/" exact component = {StreamList}/>
             <Route path ="/streams/new" exact component = {StreamCreate}/>
             <Route path ="/streams/edit/:id" exact component = {StreamEdit}/>
             <Route path ="/streams/delete/:id" exact component = {StreamDelete}/>
             <Route path ="/streams/:id" exact component = {StreamShow}/>
+        </Switch>
             </div>
+
         </Router>
     
         </div>
